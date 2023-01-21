154d6cdaec9c363e5abb4ed924369e14a8ecea3c
Richard <rich@brickbots.com>
2023-01-20 18:00:24 -0800

    Adjust setup for non-interactive use

--- pifinder_setup.sh
+++ pifinder_setup.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-sudo apt install -y git python3-pip
+sudo apt -y install git python3-pip
 git clone https://github.com/brickbots/PiFinder.git
 cd PiFinder
 sudo pip install -r requirements.txt
