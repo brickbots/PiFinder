8c8f60f30fd60cd0b6b3269e1c53ca148abe3d23
Richard <rich@brickbots.com>
2023-01-22 13:10:32 -0800

    Adding systemd stuff

--- pifinder_setup.sh
+++ pifinder_setup.sh
@@ -3,8 +3,23 @@ sudo apt-get install -y git python3-pip
 git clone https://github.com/brickbots/PiFinder.git
 cd PiFinder
 sudo pip install -r requirements.txt
+
+# Hipparcos catalog
+wget -O /home/pifinder/PiFinder/astro_data/hip_main.dat https://cdsarc.cds.unistra.fr/ftp/cats/I/239/hip_main.dat
+
+# Tetra3 solver
 cd python/PiFinder
 git clone https://github.com/esa/tetra3.git
+
+# Enable interfaces
 echo "dtparam=spi=on" | sudo tee -a /boot/config.txt
 echo "dtparam=i2c_arm=on" | sudo tee -a /boot/config.txt
 echo "dtparam=i2c_arm_baudrate=10000" | sudo tee -a /boot/config.txt
+
+# Enable service
+sudo cp /home/pifinder/PiFinder/pifinder.service /etc/systemd/system/multi-user.target.wants/pifinder.service
+sudo systemctl daemon-reload
+sudo systemctl enable pifinder
+
+echo "PiFinder setup complete, please restart the Pi"
+
