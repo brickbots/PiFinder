ef0db2dc7d78def171d458d14d0c3e593dbe3558
Richard <rich@brickbots.com>
2023-03-09 19:30:13 -0800

    Starting skysafari integration

---
+++ python/PiFinder/pos_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+This module is runs a lightweight
+server to accept socket connections
+and report telescope position
+Protocol based on ServoCat
+"""
+
+def run_server(shared_state):
+    """
+    Answers request with info from shared state
+    """
+
