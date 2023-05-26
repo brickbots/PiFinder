3de9c82a99cff44a7fb8240f372cc60630d6f33d
Mike Rosseel <mike.rosseel@gmail.com>
2023-05-26 18:14:42 +0200

    asi zwo camera, doesn't work on mac m1

--- python/PiFinder/camera_asi.py
+++ python/PiFinder/camera_asi.py
@@ -8,52 +8,74 @@ This module is the camera
 * Takes full res images on demand
 
 """
-import os
-import queue
 import time
 from PIL import Image
 from PiFinder import config
 from PiFinder import utils
 from PiFinder.camera_interface import CameraInterface
+from pathlib import Path
+from shutil import copyfile
+import time
+from PiFinder.camera_interface import CameraInterface
+import zwoasi as asi
+from typing import Dict, Tuple
+import logging
 
 
 class CameraASI(CameraInterface):
     """The camera class for PI cameras.  Implements the CameraInterface interface."""
 
     def __init__(self) -> None:
-        from picamera2 import Picamera2
-        self.camera = Picamera2()
-        # Figure out camera type, hq or gs (global shutter)
-        self.camera_type = "hq"
-        gain_mult = 1
-        self.sensor_modes = camera.sensor_modes
-        if len(sensor_modes) == 1:
-            gain_mult = 3
-            camera_type = "gs"
-        self.camType = f"PI {camera_type}"
+        import zwoasi as asi
+        # find a camera
+        # asi.init("/lib/zwoasi/armv7/libASICamera2.so")  # Initialize the ASI library
+        asi.init("/Users/mike/Downloads/ASI_Camera_SDK/ASI_linux_mac_SDK_V1.29/lib/armv8/libASICamera2.so")  # Initialize the ASI library
+        num_cameras = asi.get_num_cameras()  # Get the number of connected cameras
+        if num_cameras == 0:
+            self.handpad.display("Error:", " no camera found", "")
+            self.camType = "not found"
+            logging.info("camera not found")
+            time.sleep(1)
+        else:
+            asi.list_cameras()
+            self.initialize()
+            self.handpad.display("ZWO camera found", "", "")
+            logging.info("ZWO camera found")
+            time.sleep(1)
+        self.camType = "ZWO"
 
     def initialize(self) -> None:
         """Initializes the camera and set the needed control parameters"""
-        self.camera.stop()
-        # using this smaller scale auto-selects binning on the sensor...
-        cam_config = self.camera.create_still_configuration({"size": (512, 512)})
-        self.camera.configure(cam_config)
-        self.camera.set_controls({"AeEnable": False})
-        self.camera.set_controls({"AnalogueGain": analog_gain})
-        self.camera.set_controls({"ExposureTime": exposure_time})
-        self.camera.start()
+        if self.camType == "not found":
+            return
+        self.camera = asi.Camera(0)  # Initialize the camera
+        self.camera.set_control_value(
+            asi.ASI_BANDWIDTHOVERLOAD,
+            self.camera.get_controls()["BandWidth"]["MinValue"],
+        )
+        self.camera.disable_dark_subtract()
+        self.camera.set_control_value(asi.ASI_WB_B, 99)
+        self.camera.set_control_value(asi.ASI_WB_R, 75)
+        self.camera.set_control_value(asi.ASI_GAMMA, 50)
+        self.camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
+        self.camera.set_control_value(asi.ASI_FLIP, 0)
+        self.camera.set_image_type(asi.ASI_IMG_RAW8)
+
 
     def capture(self) -> Image.Image:
-        return self.camera.capture_image()
+        self.camera.set_control_value(asi.ASI_GAIN, self.gain)
+        self.camera.set_control_value(asi.ASI_EXPOSURE, self.exposure_time)  # microseconds
+        return Image.fromarray(self.camera.capture().astype('uint8'), 'RGB')
 
     def capture_file(self, filename) -> None:
-        return self.camera.capture_file(filename)
+        self.camera.set_control_value(asi.ASI_GAIN, gain)
+        self.camera.set_control_value(asi.ASI_EXPOSURE, exposure_time)  # microseconds
+        self.camera.capture(filename=filename)
 
-    def set_camera_config(self, exposure_time: float, gain: float) -> None:
-        self.camera.stop()
-        self.camera.set_controls({"AnalogueGain": gain})
-        self.camera.set_controls({"ExposureTime": exposure_time})
-        self.camera.start()
+    def set_camera_config(self, exposure_time: float, gain: float) -> Tuple[float, float, float]:
+        self.exposure_time = exposure_time
+        self.gain = gain
+        return exposure_time, gain, gain
 
     def get_cam_type(self) -> str:
         return self.camType
