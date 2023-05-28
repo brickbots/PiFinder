249043e535af58a1b4a7ee96f427aef1fa65ceb1
Mike Rosseel <mike.rosseel@gmail.com>
2023-05-29 00:31:34 +0200

    trying to fix the pyhotkey issue

--- python/PiFinder/keyboard_local.py
+++ python/PiFinder/keyboard_local.py
@@ -1,42 +1,51 @@
 from PiFinder.keyboard_interface import KeyboardInterface
+import logging
+try:
+    # PyHotKey doesn't seem to work on the Pi
+    from PyHotKey import Key, keyboard_manager as manager
+except ImportError:
+    pass
 
 
 class KeyboardLocal(KeyboardInterface):
 
     def __init__(self, q):
-        # PyHotKey doesn't seem to work on the Pi
-        from PyHotKey import Key, keyboard_manager as manager
-        self.q = q
-        manager.set_wetkey_on_release(Key.enter, self.callback, self.ENT)
-        manager.set_wetkey_on_release(Key.up, self.callback, self.UP)
-        manager.set_wetkey_on_release(Key.down, self.callback, self.DN)
-        manager.set_wetkey_on_release('a', self.callback, self.A)
-        manager.set_wetkey_on_release('b', self.callback, self.B)
-        manager.set_wetkey_on_release('c', self.callback, self.C)
-        manager.set_wetkey_on_release('d', self.callback, self.D)
-        manager.set_wetkey_on_release('0', self.callback, 0)
-        manager.set_wetkey_on_release('1', self.callback, 1)
-        manager.set_wetkey_on_release('2', self.callback, 2)
-        manager.set_wetkey_on_release('3', self.callback, 3)
-        manager.set_wetkey_on_release('4', self.callback, 4)
-        manager.set_wetkey_on_release('5', self.callback, 5)
-        manager.set_wetkey_on_release('6', self.callback, 6)
-        manager.set_wetkey_on_release('7', self.callback, 7)
-        manager.set_wetkey_on_release('8', self.callback, 8)
-        manager.set_wetkey_on_release('9', self.callback, 9)
-        manager.register_hotkey([Key.enter, Key.up], None, self.callback, self.ALT_UP)
-        manager.register_hotkey([Key.enter, Key.down], None, self.callback, self.ALT_DN)
-        manager.register_hotkey([Key.enter, 'a'], None, self.callback, self.ALT_A)
-        manager.register_hotkey([Key.enter, 'b'], None, self.callback, self.ALT_B)
-        manager.register_hotkey([Key.enter, 'c'], None, self.callback, self.ALT_C)
-        manager.register_hotkey([Key.enter, 'd'], None, self.callback, self.ALT_D)
-        manager.register_hotkey([Key.enter, '0'], None, self.callback, self.ALT_0)
-        manager.register_hotkey([Key.shift, 'a'], None, self.callback, self.LNG_A)
-        manager.register_hotkey([Key.shift, 'b'], None, self.callback, self.LNG_B)
-        manager.register_hotkey([Key.shift, 'c'], None, self.callback, self.LNG_C)
-        manager.register_hotkey([Key.shift, 'd'], None, self.callback, self.LNG_D)
-        manager.register_hotkey([Key.shift, Key.enter], None, self.callback, self.LNG_ENT)
+        logging.debug('KeyboardLocal.__init__')
+        try:
+            self.q = q
+            manager.set_wetkey_on_release(Key.enter, self.callback, self.ENT)
+            manager.set_wetkey_on_release(Key.up, self.callback, self.UP)
+            manager.set_wetkey_on_release(Key.down, self.callback, self.DN)
+            manager.set_wetkey_on_release('a', self.callback, self.A)
+            manager.set_wetkey_on_release('b', self.callback, self.B)
+            manager.set_wetkey_on_release('c', self.callback, self.C)
+            manager.set_wetkey_on_release('d', self.callback, self.D)
+            manager.set_wetkey_on_release('0', self.callback, 0)
+            manager.set_wetkey_on_release('1', self.callback, 1)
+            manager.set_wetkey_on_release('2', self.callback, 2)
+            manager.set_wetkey_on_release('3', self.callback, 3)
+            manager.set_wetkey_on_release('4', self.callback, 4)
+            manager.set_wetkey_on_release('5', self.callback, 5)
+            manager.set_wetkey_on_release('6', self.callback, 6)
+            manager.set_wetkey_on_release('7', self.callback, 7)
+            manager.set_wetkey_on_release('8', self.callback, 8)
+            manager.set_wetkey_on_release('9', self.callback, 9)
+            manager.register_hotkey([Key.enter, Key.up], None, self.callback, self.ALT_UP)
+            manager.register_hotkey([Key.enter, Key.down], None, self.callback, self.ALT_DN)
+            manager.register_hotkey([Key.enter, 'a'], None, self.callback, self.ALT_A)
+            manager.register_hotkey([Key.enter, 'b'], None, self.callback, self.ALT_B)
+            manager.register_hotkey([Key.enter, 'c'], None, self.callback, self.ALT_C)
+            manager.register_hotkey([Key.enter, 'd'], None, self.callback, self.ALT_D)
+            manager.register_hotkey([Key.enter, '0'], None, self.callback, self.ALT_0)
+            manager.register_hotkey([Key.shift, 'a'], None, self.callback, self.LNG_A)
+            manager.register_hotkey([Key.shift, 'b'], None, self.callback, self.LNG_B)
+            manager.register_hotkey([Key.shift, 'c'], None, self.callback, self.LNG_C)
+            manager.register_hotkey([Key.shift, 'd'], None, self.callback, self.LNG_D)
+            manager.register_hotkey([Key.shift, Key.enter], None, self.callback, self.LNG_ENT)
+        except Exception as e:
+            logging.error('KeyboardLocal.__init__: {}'.format(e))
         # manager.logger = True
+        logging.debug('KeyboardLocal.__init__')
 
     def callback(self, key):
         self.q.put(key)
