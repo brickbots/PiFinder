f690675b6296c391e36d9efe738de7f1d210597e
Richard <rich@brickbots.com>
2023-07-05 18:37:00 -0700

    Post update switch of tetra3 repo

--- pifinder_post_update.sh
+++ pifinder_post_update.sh
@@ -1,5 +1,17 @@
+# Switch tetra 3 to submodule
+if test -f "/home/pifinder/PiFinder/python/PiFinder/tetra3/default_database.np"; then
+    echo "Switching Tetra3"
+    rm -rm /home/pifinder/PiFinder/python/PiFinder/tetra3
+    git submodule --init --update
+fi
+
+# GPSD
 sudo apt install -y gpsd
 sudo dpkg-reconfigure -plow gpsd
+
+# PWM
 sudo sed -zi '/dtoverlay=pwm,pin=13,func=4\n/!s/$/\ndtoverlay=pwm,pin=13,func=4\n/' /boot/config.txt
+
+# DONE
 echo "Post Update Complete"
 
