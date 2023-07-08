e6e0fcf7bc92b3ae7c1deb3958865794c67bb21c
Richard <rich@brickbots.com>
2023-07-08 13:49:34 -0700

    Simplify switch to submodule

--- pifinder_post_update.sh
+++ pifinder_post_update.sh
@@ -1,10 +1,3 @@
-# Switch tetra 3 to submodule
-if test -f "/home/pifinder/PiFinder/python/PiFinder/tetra3/default_database.npz"; then
-    echo "Switching Tetra3"
-    rm -rf /home/pifinder/PiFinder/python/PiFinder/tetra3
-    git submodule update --init
-fi
-
 # GPSD
 sudo apt install -y gpsd
 sudo dpkg-reconfigure -plow gpsd
