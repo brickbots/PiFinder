ac490b06d495b907da02a183c5a22d81d4df5f4b
Richard <rich@brickbots.com>
2023-08-06 14:33:26 -0700

    relocate observations.db and migrate on post update

--- python/PiFinder/utils.py
+++ python/PiFinder/utils.py
@@ -16,4 +16,4 @@ pifinder_dir = Path("..")
 astro_data_dir = pifinder_dir / "astro_data"
 data_dir = Path(Path.home(), "PiFinder_data")
 pifinder_db = astro_data_dir / "pifinder_objects.db"
-observations_db = astro_data_dir / "observations.db"
+observations_db = data_dir / "observations.db"
