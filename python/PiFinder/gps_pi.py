adb5115960fff1f5d15b475257e489195b5c0fec
Mike Rosseel <mike.rosseel@gmail.com>
2023-09-16 00:16:46 +0200

    Use sep as fallback, relax to 500m error

--- python/PiFinder/gps_pi.py
+++ python/PiFinder/gps_pi.py
@@ -14,11 +14,11 @@ def is_tpv_accurate(tpv_dict):
     """
     Check the accuracy of the GPS fix
     """
-    # logging.debug("GPS: TPV: mode=%s, acc=%s",  tpv_dict.get('mode'), tpv_dict.get('ecefpAcc'))
-    if tpv_dict.get("mode") == 3 and tpv_dict.get("ecefpAcc") < 100:
+    error = tpv_dict.get("ecepAcc", tpv_dict.get("sep", 499))
+    #logging.debug("GPS: TPV: mode=%s, error=%s",  tpv_dict.get('mode'), error)
+    if tpv_dict.get("mode") == 3 and error < 500: 
         return True
     else:
-        # logging.debug("GPS: TPV accuracy not good enough: mode=%s, acc=%s",  tpv_dict.get('mode'), tpv_dict.get('ecefpAcc'))
         return False
 
 
@@ -27,9 +27,6 @@ def gps_monitor(gps_queue, console_queue):
     while True:
         with GPSDClient(host="127.0.0.1") as client:
             # see https://www.mankier.com/5/gpsd_json for the list of fields
-            # readings_filter = filter(lambda x : is_tpv_accurate(x), client.dict_stream(convert_datetime=True, filter=["TPV"]))
-            # readings = list(readings_filter)
-            # logging.debug(f"GPS has {len(readings)} readings")
             while True:
                 logging.debug("GPS waking")
                 readings_filter = filter(
