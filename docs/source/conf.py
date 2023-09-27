829bfb74b79865735220a99159e21b488127fd63
Richard <rich@brickbots.com>
2023-09-27 16:24:13 -0700

    Continuing RTD / Sphinx setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,14 @@ extensions = []
 templates_path = ["_templates"]
 exclude_patterns = []
 
+extensions = [
+    "sphinx_rtd_theme",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {"navigation_depth": 3}
+
 html_static_path = ["_static"]
