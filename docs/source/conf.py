30f04d3f4834a331caa67076e0c185519cf6cdf3
Richard <rich@brickbots.com>
2023-09-27 15:47:03 -0700

    Sphinx setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PiFinder'
-copyright = '2023, Richard Wolff-Jacobson'
-author = 'Richard Wolff-Jacobson'
+project = "PiFinder"
+copyright = "2023, Richard Wolff-Jacobson"
+author = "Richard Wolff-Jacobson"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
