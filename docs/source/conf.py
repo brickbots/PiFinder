63e87d3279ac20e6384ac5b81bbcf7962805b32e
Richard <rich@brickbots.com>
2023-09-30 11:58:14 -0700

    PDJ for the win

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,12 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = "sphinx_rtd_theme"
-html_theme = "groundwork"
-html_theme_options = {"navigation_depth": 3}
-
+html_theme = "sphinx_pdj_theme"
+#html_theme_options = {"navigation_depth": 3}
+#html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_logo = "images/WebLogo_RED.png"
 html_static_path = ["_static"]
+html_theme_options = {
+    'home_link': 'hide',
+    "prefers-color-scheme": "light",
+}
