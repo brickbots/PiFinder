e2d0eaeb63198ca0d1552b140fcdf29373fc603a
Richard <rich@brickbots.com>
2023-12-15 18:06:49 -0800

    Move to venv

--- pifinder_update.sh
+++ pifinder_update.sh
@@ -2,7 +2,8 @@
 git checkout release
 git pull
 git submodule update --init --recursive
-sudo pip install -r /home/pifinder/PiFinder/requirements.txt
+source /home/pifinder/PiFinder/python/venv/bin/activate
+pip install -r /home/pifinder/PiFinder/requirements.txt
 source /home/pifinder/PiFinder/pifinder_post_update.sh
 
 echo "PiFinder software update complete, please restart the Pi"
