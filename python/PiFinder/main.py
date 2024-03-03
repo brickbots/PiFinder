94b9e96f7338f66619e7d3f4aa8764ab073f0239
Richard <rich@brickbots.com>
2024-03-02 16:34:14 -0800

    Update mode indexes for Nearby addition

--- python/PiFinder/main.py
+++ python/PiFinder/main.py
@@ -419,9 +419,9 @@ def main(script_name=None, show_fps=False, verbose=False):
 
         # What is the highest index for observing modes
         # vs status/debug modes accessed by alt-A
-        ui_observing_modes = 4
-        ui_mode_index = 4
-        logging_mode_index = 7
+        ui_observing_modes = 5
+        ui_mode_index = 5
+        logging_mode_index = 8
 
         current_module = ui_modes[ui_mode_index]
 
