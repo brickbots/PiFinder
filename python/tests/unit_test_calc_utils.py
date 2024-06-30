2a79b67ec4fbea4073ad0c99a282c8356e615724
Tak Kaneko <>
2024-06-30 17:29:49 +0200

    Add test: test_hadec_to_pa()

--- python/tests/unit_test_calc_utils.py
+++ python/tests/unit_test_calc_utils.py
@@ -14,7 +14,7 @@ class UnitTestCalcUtils(unittest.TestCase):
     """
 
     def test_hadec_to_pa0(self):
-        """ Test hadec_to_pa() for the special case when HA = 0 """
+        """ Unit test hadec_to_pa() for the special case when HA = 0 """
         # Define the inputs:
         ha_deg = 0.0
         lat_deg = 51.0  # Approximately Greenwich Observatory
@@ -30,26 +30,26 @@ class UnitTestCalcUtils(unittest.TestCase):
                 self.assertAlmostEqual(pa_deg, 0.0, places=3, 
                                        msg='HA = 0: dec={:.1f}'.format(dec))
     
-"""
+
     def test_hadec_to_pa(self):
+        """ Unit test haddec_to_pa() for when HA != 0 """
         # Define the inputs:        
         ha_deg = 60.0
         lat_deg = 51.0  # Approximately Greenwich Observatory
         dec_degs = [90, 60, 51, 30, 0, -30]
-        # Expected values
-        exp_pa_degs = []
-        
-        expected_pa_degs = []  # For HA=+60 deg
+        # Expected values for +ve HA (exp. values for -ve HA are the -ves)
+        expected_pa_degs = [120.00000, 77.9774, 65.8349, 46.5827, 35.0417, 33.2789]
+
         for dec, expected in zip(dec_degs, expected_pa_degs):
             # +ve HA case:
             pa_deg = hadec_to_pa(ha_deg, dec, lat_deg)
             self.assertAlmostEqual(pa_deg, expected, places=3, 
-                                   message='HA = {:.2f}, dec = {:.2f}'.format(ha_deg, dec))
+                                   msg='HA = {:.2f}, dec = {:.2f}'.format(ha_deg, dec))
             # -ve HA case (expect -ve values):
             pa_deg = hadec_to_pa(-ha_deg, dec, lat_deg)
             self.assertAlmostEqual(pa_deg, -expected, places=3, 
-                                   message='HA = {:.2f}, dec = {:.2f}'.format(-ha_deg, dec))
-"""
+                                   msg='HA = {:.2f}, dec = {:.2f}'.format(-ha_deg, dec))
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
