cbef575d18e3635cf47967888d83e192ab9669b5
Mike Rosseel <mike.rosseel@gmail.com>
2024-09-06 18:11:41 +0200

    revert gps fixes in this branch

--- python/PiFinder/gps_pi.py
+++ python/PiFinder/gps_pi.py
@@ -2,94 +2,93 @@
 # -*- coding:utf-8 -*-
 """
 This module is for GPS related functions
+
 """
+
 import time
 from PiFinder.multiproclogging import MultiprocLogging
 from gpsdclient import GPSDClient
 import logging
-import json
+from itertools import islice
 
 logger = logging.getLogger("GPS")
+
 
 def is_tpv_accurate(tpv_dict):
     """
     Check the accuracy of the GPS fix
     """
-    mode = tpv_dict.get("mode", 0)
-    error = tpv_dict.get("epx", tpv_dict.get("epy", 499))  # Using horizontal error
+    error = tpv_dict.get("ecefpAcc", tpv_dict.get("sep", 499))
     logger.debug(
-        "GPS: TPV: mode=%s, error=%s, lat=%s, lon=%s",
-        mode,
+        "GPS: TPV: mode=%s, error=%s, ecefpAcc=%s, sep=%s",
+        tpv_dict.get("mode"),
         error,
-        tpv_dict.get("lat"),
-        tpv_dict.get("lon"),
+        tpv_dict.get("ecefpAcc", -1),
+        tpv_dict.get("sep", -1),
     )
-    return mode >= 2 and error < 500
+    if tpv_dict.get("mode") >= 2 and error < 500:
+        return True
+    else:
+        return False
+
 
 def gps_monitor(gps_queue, console_queue, log_queue):
     MultiprocLogging.configurer(log_queue)
     gps_locked = False
-    last_sky_update = 0
+    while True:
+        with GPSDClient(host="127.0.0.1") as client:
+            # see https://www.mankier.com/5/gpsd_json for the list of fields
+            while True:
+                logger.debug("GPS waking")
+                readings_filter = filter(
+                    lambda x: is_tpv_accurate(x),
+                    client.dict_stream(convert_datetime=True, filter=["TPV"]),
+                )
+                sky_filter = client.dict_stream(convert_datetime=True, filter=["SKY"])
+                readings_list = list(islice(readings_filter, 10))
+                sky_list = list(islice(sky_filter, 10))
+                if readings_list:
+                    result = min(
+                        readings_list,
+                        key=lambda x: x.get("ecefpAcc", x.get("sep", float("inf"))),
+                    )
+                    logger.debug("last reading is %s", result)
+                    if result.get("lat") and result.get("lon") and result.get("altHAE"):
+                        if gps_locked is False:
+                            gps_locked = True
+                            console_queue.put("GPS: Locked")
+                            logger.debug("GPS locked")
+                        msg = (
+                            "fix",
+                            {
+                                "lat": result.get("lat"),
+                                "lon": result.get("lon"),
+                                "altitude": result.get("altHAE"),
+                            },
+                        )
+                        logger.debug("GPS fix: %s", msg)
+                        gps_queue.put(msg)
 
-    with GPSDClient(host="127.0.0.1") as client:
-        while True:
-            logger.debug("GPS waking")
+                    # search from the newest first, quit if something is found
+                    for result in reversed(readings_list):
+                        if result.get("time"):
+                            msg = ("time", result.get("time"))
+                            logger.debug("Setting time to %s", result.get("time"))
+                            gps_queue.put(msg)
+                            break
+                else:
+                    logger.debug("GPS TPV client queue is empty")
 
-            try:
-                for msg in client.json_stream():
-                    current_time = time.time()
-
-                    try:
-                        parsed_msg = json.loads(msg)
-                    except json.JSONDecodeError:
-                        logger.error(f"Failed to parse JSON: {msg}")
-                        continue
-
-                    logger.debug(f"Received message: {parsed_msg.get('class')}")
-
-                    if parsed_msg.get('class') == 'TPV':
-                        if is_tpv_accurate(parsed_msg):
-                            if parsed_msg.get("lat") and parsed_msg.get("lon"):
-                                if not gps_locked:
-                                    gps_locked = True
-                                    console_queue.put("GPS: Locked")
-                                    logger.debug("GPS locked")
-
-                                fix_msg = (
-                                    "fix",
-                                    {
-                                        "lat": parsed_msg.get("lat"),
-                                        "lon": parsed_msg.get("lon"),
-                                        "altitude": parsed_msg.get("alt", 0),  # Use 'alt' for 2D fix
-                                    },
-                                )
-                                logger.debug("GPS fix: %s", fix_msg)
-                                gps_queue.put(fix_msg)
-
-                            if parsed_msg.get("time"):
-                                time_msg = ("time", parsed_msg.get("time"))
-                                logger.debug("Setting time to %s", parsed_msg.get("time"))
-                                gps_queue.put(time_msg)
-
-                    elif parsed_msg.get('class') == 'SKY':
-                        if current_time - last_sky_update >= 7:  # Update every 7 seconds
-                            satellites = parsed_msg.get("satellites", [])
-                            sats_seen = len(satellites)
-                            sats_used = sum(1 for sat in satellites if sat.get("used"))
+                if sky_list:
+                    # search from the newest first, quit if something is found
+                    for result in reversed(sky_list):
+                        if result["class"] == "SKY" and "nSat" in result:
+                            sats_seen = result["nSat"]
+                            sats_used = result["uSat"]
                             num_sats = (sats_seen, sats_used)
-                            sat_msg = ("satellites", num_sats)
-                            logger.debug("Number of sats seen/used: %i/%i", sats_seen, sats_used)
-                            gps_queue.put(sat_msg)
-                            last_sky_update = current_time
-
-                    # Break the inner loop after processing a batch of messages
-                    if current_time - last_sky_update >= 7:
-                        break
-
-            except Exception as e:
-                logger.error("Error in GPS monitor: %s", str(e))
-
-            logger.debug("GPS sleeping now for 1s")
-            time.sleep(1)  # Sleep for a short time before checking for new messages
-
-        logger.warning("GPS monitor exited unexpectedly")
+                            msg = ("satellites", num_sats)
+                            logger.debug("Number of sats seen: %i", num_sats)
+                            gps_queue.put(msg)
+                            break
+                logger.debug("GPS sleeping now for 7s")
+                time.sleep(7)
