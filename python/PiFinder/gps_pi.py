882fc50ce99294b4f9317bbd16a599440ef4c071
Mike Rosseel <mike.rosseel@gmail.com>
2024-09-16 22:41:23 +0200

    hdop and pdop getting it from sky

--- python/PiFinder/gps_pi.py
+++ python/PiFinder/gps_pi.py
@@ -10,22 +10,26 @@ import logging
 
 logger = logging.getLogger("GPS")
 
+error_2d = 999
+error_3d = 999
+
 
 def is_tpv_accurate(tpv_dict):
     """
     Check the accuracy of the GPS fix
     """
+    global error_2d, error_3d
     # get the ecefpAcc if present, else get sep, else use 499
-    error = tpv_dict.get("ecefpAcc", tpv_dict.get("sep", 499))
+    # error = tpv_dict.get("ecefpAcc", tpv_dict.get("sep", 499))
     mode = tpv_dict.get("mode")
-    error_2d = tpv_dict.get("hdop", 999)
-    error_3d = tpv_dict.get("pdop", 999)
     logger.debug(
         "GPS: TPV: mode=%s, error=%s, ecefpAcc=%s, sep=%s",
         mode,
-        error,
+        # error,
         tpv_dict.get("ecefpAcc", -1),
         tpv_dict.get("sep", -1),
+        error_2d,
+        error_3d,
     )
     if mode == 2 and error_2d < 1000:
         return True
@@ -44,9 +48,13 @@ async def aiter_wrapper(sync_iter):
 
 async def process_sky_messages(client, gps_queue):
     sky_stream = client.dict_stream(filter=["SKY"])
+    global error_2d, error_3d
     async for result in aiter_wrapper(sky_stream):
         logger.debug(
             "GPS: SKY: %s", result)
+        if result["class"] == "SKY":
+            error_2d = result.get("hdop", 999)
+            error_3d = result.get("pdop", 999)
         if result["class"] == "SKY" and "nSat" in result:
             sats_seen = result["nSat"]
             sats_used = result["uSat"]
