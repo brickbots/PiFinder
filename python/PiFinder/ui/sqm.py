34dc9e4a16658ad63eaaf7c612ee6c0b98b0b228
Mike Rosseel <mike.rosseel@gmail.com>
2024-10-22 17:30:13 +0200

    Protect against various crash situations

--- python/PiFinder/ui/sqm.py
+++ python/PiFinder/ui/sqm.py
@@ -23,21 +23,20 @@ class UISQM(UIModule):
         sleep_for_framerate(self.shared_state)
         self.clear_screen()
 
-        if (self.shared_state is None or self.shared_state.solution is None or self.shared_state.solution()["SQM"] is None):
+        if (self.shared_state.solve_state is None or self.shared_state.solution() is None or self.shared_state.solution()["SQM"] is None):
             self.draw.text(
                 (10, 30),
                 "NO SQM DATA",
-                font=self.fonts.bold_font,
+                font=self.fonts.bold.font,
                 fill=self.colors.get(128),
             )
-            return self.screen_update()
-
-        self.draw.text(
-            (10, 30),
-            f"{self.shared_state.solution()['SQM'][0]:.2f}",
-            font=self.fonts.huge.font,
-            fill=self.colors.get(128),
-        )
+        else:
+            self.draw.text(
+                (10, 30),
+                f"{self.shared_state.solution()['SQM'][0]:.2f}",
+                font=self.fonts.huge.font,
+                fill=self.colors.get(128),
+            )
         return self.screen_update()
 
     # def key_up(self):
