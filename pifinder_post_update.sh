442fe41b0bcc7c65239ca124b1921fdca4c6a91d
Richard <rich@brickbots.com>
2024-11-30 13:37:38 -0800

    working on conditionals

--- pifinder_post_update.sh
+++ pifinder_post_update.sh
@@ -2,14 +2,14 @@ git submodule update --init --recursive
 sudo pip install -r /home/pifinder/PiFinder/python/requirements.txt
 
 # Set up migrations folder if it does not exist
-if ! [ -d "/home/pifinder/PiFinder_data/migrations"]
+if ! [ -d "/home/pifinder/PiFinder_data/migrations" ]
 then
     mkdir /home/pifinder/PiFinder_data/migrations
 fi
 
 # v1.x.x
 # everying prior to selecitve migrations
-if ! [ -f "/home/pifinder/PiFinder_data/migrations/v1.x.x"]
+if ! [ -f "/home/pifinder/PiFinder_data/migrations/v1.x.x" ]
 then
     source /home/pifinder/PiFinder/migration_source/v1.x.x.sh
     touch /home/pifinder/PiFinder_data/migrations/v1.x.x
@@ -17,7 +17,7 @@ fi
 
 # v2.1.0
 # Switch to Cedar
-if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.1.0"]
+if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.1.0" ]
 then
     source /home/pifinder/PiFinder/migration_source/v2.1.0.sh
     touch /home/pifinder/PiFinder_data/migrations/v2.1.0
