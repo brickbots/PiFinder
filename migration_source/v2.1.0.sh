a9c5683fefffa9fb1133927efad2a37fc6e1f2e6
Richard <rich@brickbots.com>
2024-12-01 19:14:45 -0800

    Simplify tetra3 update

--- migration_source/v2.1.0.sh
+++ migration_source/v2.1.0.sh
@@ -1,10 +1,6 @@
 # swap tetra3 submodule
-#git submodule set-url -- python/PiFinder/tetra3 https://github.com/smroid/cedar-solve
 git submodule sync
 git submodule update --init --recursive
-#cd /home/pifinder/PiFinder/python/PiFinder/tetra3
-#git checkout 38c3f48
-#git pull
 
 # Set up symlink
 ln -s /home/pifinder/PiFinder/python/PiFinder/tetra3/tetra3 /home/pifinder/PiFinder/python/tetra3
