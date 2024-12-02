7543caa4ec199e5aef282d8b07a4c506b79c7ffb
Richard <rich@brickbots.com>
2024-12-01 18:32:05 -0800

    Remove mypy ignore

--- python/PiFinder/solver.py
+++ python/PiFinder/solver.py
@@ -20,8 +20,8 @@ from PiFinder import state_utils
 from PiFinder import utils
 
 sys.path.append(str(utils.tetra3_dir))
-import PiFinder.tetra3.tetra3 as tetra3  # type: ignore
-from PiFinder.tetra3.tetra3 import cedar_detect_client  # type: ignore
+import PiFinder.tetra3.tetra3 as tetra3
+from PiFinder.tetra3.tetra3 import cedar_detect_client
 
 logger = logging.getLogger("Solver")
 
