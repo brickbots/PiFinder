62557b130bf35e17a9da483f85ccae22d673e31e
Jens Scheidtmann <Jens.Scheidtmann@gmail.com>
2025-01-12 17:19:42 +0100

    Prototype go at extracted string for i18n

--- python/PiFinder/main.py
+++ python/PiFinder/main.py
@@ -10,6 +10,13 @@ This module is the main entry point for PiFinder it:
 
 """
 
+# Install the _("text") into global context for Internationalization
+# On RasPi/Ubuntu the default locale is C.utf8, see `locate -a`, which locales are available
+# You need to install `apt install language-pack_xx`, where xx is the ISO country code.
+# Passing nothing as third parameter means the language is determined from environment variables (e.g. LANG)
+import gettext
+gettext.install('PiFinder', 'locale', '')
+
 import os
 
 # skyfield performance fix, see: https://rhodesmill.org/skyfield/accuracy-efficiency.html
