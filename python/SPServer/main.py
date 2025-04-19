9ea2e4f994307c77e3e017711b1bacea5969840d
Richard <rich@brickbots.com>
2025-04-18 19:49:14 -0700

    Adding commands

--- python/SPServer/main.py
+++ python/SPServer/main.py
@@ -6,23 +6,74 @@ Primary module for the Star Party local server
 
 import asyncio
 import uuid
-from random import random
+from random import random, choice
 
-from SPServer.sps_data import Observer, ServerState, Mark, Group, Position
+from SPServer.sps_data import Observer, ServerState
 
 state_lock = asyncio.Lock()
 server_state = ServerState()
 
 
-async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, observer: Observer):
+def make_group_name() -> str:
+    adjectives = [
+        "stellar",
+        "bright",
+        "dark",
+        "radiant",
+        "cosmic",
+        "galactic",
+        "wispy",
+        "nebular",
+        "variable",
+        "local",
+        "infrared",
+        "radio",
+    ]
+
+    nouns = [
+        "nova",
+        "quasar",
+        "nebula",
+        "comet",
+        "pulsar",
+        "galaxy",
+        "asteroid",
+        "scope",
+        "dwarf",
+        "cluster",
+        "planet",
+        "moon",
+        "orbit",
+        "flare",
+        "core",
+    ]
+
+    adj = choice(adjectives)
+    noun = choice(nouns)
+    return f"{adj.capitalize()}{noun.capitalize()}"
+
+
+async def writeline(writer: asyncio.StreamWriter, message: str):
+    message = message + "\n"
+    writer.write(message.encode())
+    await writer.drain()
+
+
+async def handle_command(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter, observer: Observer
+):
     global server_state
 
     connection_id = observer.connection_id
-    connection_id_short = connection_id[-4]
+    connection_id_short = connection_id[-4:]
 
     while True:
         in_data_raw = await reader.readline()
-        in_data = in_data_raw.decode()
+        try:
+            in_data = in_data_raw.decode().strip()
+        except UnicodeDecodeError:
+            in_data = "error"
+
         if in_data == "":
             # This indicates EOF/Reader closed
             break
@@ -30,23 +81,40 @@ async def handle_command(reader: asyncio.StreamReader, writer: asyncio.StreamWri
         print(f"{connection_id_short}:{in_data}")
         command = in_data.split("|")
         if command[0] == "echo":  # echo
-            writer.write(command[1].encode())
-            await writer.drain()
+            await writeline(writer, command[1])
 
         if command[0] == "pos":  # set position
-            with state_lock:
-                observer.position.ra=float(command[1])
-                observer.position.dec=float(command[2])
+            async with state_lock:
+                observer.position.ra = float(command[1])
+                observer.position.dec = float(command[2])
 
-        if command[0] == "groups": # list groups
-            
+        if command[0] == "groups":  # list groups
+            for group_touple in server_state.list_groups():
+                _out_line = f"{group_touple[0]}|{group_touple[1]}\n"
+                writer.write(_out_line.encode())
+                await writer.drain()
+
+        if command[0] == "add_group":  # Add new group
+            new_group_name = make_group_name()
+            async with state_lock:
+                new_group = server_state.add_group(observer, new_group_name)
+            await writeline(writer, new_group.name)
+
+        if command[0] == "join":  # join group
+            group_name = command[1]
+            with state_lock:
+                result = server_state.join_group(observer, group_name)
+
+            if result:
+                await writeline(writer, "ack")
+            else:
+                await writeline(writer, "err")
 
 
 async def send_pos_updates(writer: asyncio.StreamWriter, observer: Observer):
-
     global server_state
     while True:
-        if random() > 0.9:
+        if random() > 1:
             writer.write("pos|20|20\n".encode())
             try:
                 await writer.drain()
@@ -60,12 +128,13 @@ async def client_connected(reader: asyncio.StreamReader, writer: asyncio.StreamW
     global server_state
 
     connection_id = str(uuid.uuid4())
-    connection_id_short = connection_id[-4]
+    connection_id_short = connection_id[-4:]
     print(f"{connection_id_short}: Attempt")
     in_data_raw = await reader.readline()
     in_data = in_data_raw.decode()
     command = in_data.split("|")
     if len(command) != 2 or command[0] != "name":
+        await writeline(writer, "err")
         print(f"{connection_id_short}: Bad Connection Attempt")
         writer.close()
         await writer.wait_closed()
@@ -78,11 +147,14 @@ async def client_connected(reader: asyncio.StreamReader, writer: asyncio.StreamW
         name=observer_name,
         group=None,
     )
-    with state_lock:
+    async with state_lock:
         server_state.observers.append(observer)
 
+    await writeline(writer, "ack")
     print(f"{connection_id_short}: {observer_name} Connected")
-    await asyncio.gather(handle_command(reader, writer, observer), send_pos_updates(writer, observer))
+    await asyncio.gather(
+        handle_command(reader, writer, observer), send_pos_updates(writer, observer)
+    )
     print(f"{connection_id_short}: Closed")
 
 
--- python/SPServer/sps_data.py
+++ python/SPServer/sps_data.py
@@ -11,12 +11,15 @@ from typing import Union
 @dataclass
 class Group:
     name: str
-    marks: list["Mark"]
+    marks: list["Mark"] = field(default_factory=list)
+    observers: list["Observer"] = field(default_factory=list)
+
 
 @dataclass
 class Position:
     ra: float = 0
     dec: float = 0
+
 
 @dataclass
 class Observer:
@@ -30,6 +33,78 @@ class Observer:
 class ServerState:
     groups: list[Group] = field(default_factory=list)
     observers: list[Observer] = field(default_factory=list)
+
+    def observer_count(self, group: Group) -> int:
+        """
+        returns the count of observers in a group
+        """
+        return len(group.observers)
+
+    def add_group(self, observer: Observer, group_name: str) -> Group:
+        """
+        Adds a new group, then adds the observer
+        to it
+        """
+        if observer.group:
+            self.leave_group(observer.group)
+
+        new_group = Group(name=group_name, observers=[observer])
+        observer.group = new_group
+        return new_group
+
+    def join_group(self, observer: Observer, group_name: str) -> bool:
+        """
+        Add an observer to a group
+        """
+        _group = self.get_group_by_name(group_name)
+        if _group is None:
+            return False
+
+        if observer.group is _group:
+            # already in the group!
+            return True
+
+        self.leave_group(observer.group)
+        _group.observers.append(observer)
+        observer.group = _group
+        return True
+
+    def get_group_by_name(self, group_name: str) -> Union[None | Group]:
+        """
+        Find a group by name
+        """
+        for group in self.groups:
+            if group.name == group_name:
+                return group
+
+        return None
+
+    def leave_group(self, group: Group, observer: Observer) -> bool:
+        """
+        Called whenever someone is leaving a group.
+        If the group is empty, it's deleted
+        """
+        if observer.group != group:
+            # not in the group!
+            return False
+
+        group.observers.remove(observer)
+        observer.group = None
+        if self.observer_count(group) == 0:
+            self.groups.remove(group)
+
+        return True
+
+    def list_groups(self):
+        """
+        Returns a list of tuples with group names and
+        observer count
+        """
+        _ret_list = []
+        for group in self.groups:
+            _ret_list.append((group.name, self.observer_count(group)))
+
+        return _ret_list
 
 
 @dataclass
