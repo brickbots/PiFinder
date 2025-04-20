c82266f079f4e3c395de0c2b5987601ad260c318
Jens Scheidtmann <Jens.Scheidtmann@gmail.com>
2025-04-20 22:34:12 +0200

    Use pifinder_dir for wifi_status.txt

--- python/PiFinder/sys_utils.py
+++ python/PiFinder/sys_utils.py
@@ -31,7 +31,7 @@ class Network:
 
     @staticmethod
     def get_wifi_mode():
-        with open("/home/pifinder/PiFinder/wifi_status.txt", "r") as wifi_f:
+        with open(f"{utils.pifinder_dir}/wifi_status.txt", "r") as wifi_f:
             return wifi_f.read()
         
     @staticmethod
