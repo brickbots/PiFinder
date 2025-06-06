771047ff0cb68cda2f792fd5c8b8dd50d41d28aa
Mike Rosseel <mike.rosseel@gmail.com>
2025-06-06 21:13:40 +0200

    re-add comets

--- python/PiFinder/ui/menu_structure.py
+++ python/PiFinder/ui/menu_structure.py
@@ -89,6 +89,12 @@ pifinder_menu = {
                             "value": "PL",
                         },
                         {
+                            "name": "Comets",
+                            "class": UIObjectList,
+                            "objects": "catalog",
+                            "value": "CM",
+                        },
+                        {
                             "name": "NGC",
                             "class": UIObjectList,
                             "objects": "catalog",
