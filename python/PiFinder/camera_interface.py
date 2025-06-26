54618e71447b698542b4a51bd29753b2dc8163b4
TakKanekoGit <>
2025-06-26 20:16:15 +0100

    camera_interface.py: Reverted to pointing_diff calculation using quaternion -- works

--- python/PiFinder/camera_interface.py
+++ python/PiFinder/camera_interface.py
@@ -14,6 +14,7 @@ import queue
 import time
 from PIL import Image
 import numpy as np
+import quaternion  # Numpy quaternion
 from typing import Tuple
 import logging
 
@@ -113,19 +114,22 @@ class CameraInterface:
 
                     # see if we moved during exposure
                     pointing_diff = 0.0
-                    if imu_start and imu_end:
-                        pointing_diff = (
-                            abs(imu_start["pos"][0] - imu_end["pos"][0])
-                            + abs(imu_start["pos"][1] - imu_end["pos"][1])
-                            + abs(imu_start["pos"][2] - imu_end["pos"][2])
-                        )
+                    
+                    # TODO: REMOVE
+                    #if imu_start and imu_end:
+                    #    pointing_diff = (
+                    #        abs(imu_start["pos"][0] - imu_end["pos"][0])
+                    #        + abs(imu_start["pos"][1] - imu_end["pos"][1])
+                    #        + abs(imu_start["pos"][2] - imu_end["pos"][2])
+                    #    )
+                    
                     # # Quaternion version
-                    # if imu_start and imu_end:
-                    #     # Returns the pointing difference between successive IMU quaternions as
-                    #     # an angle (radians). Note that this also accounts for rotation around the 
-                    #     # scope axis. Returns an angle in radians.
-                    #     pointing_diff = pointing.get_quat_angular_diff(
-                    #         imu_start["quat"], imu_end["quat"])
+                    if imu_start and imu_end:
+                        # Returns the pointing difference between successive IMU quaternions as
+                        # an angle (radians). Note that this also accounts for rotation around the 
+                        # scope axis. Returns an angle in radians.
+                        pointing_diff = pointing.get_quat_angular_diff(
+                            quaternion.from_float_array(imu_start["quat"]), quaternion.from_float_array(imu_end["quat"]))
 
                     camera_image.paste(base_image)
                     shared_state.set_last_image_metadata(
