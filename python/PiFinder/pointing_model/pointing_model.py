8ab3613c2ec0ef8c3bfda79831f7225c4b2ed191
TakKanekoGit <>
2025-07-05 21:02:43 +0100

    Add new class ImuDeadReckoning

--- python/PiFinder/pointing_model/pointing_model.py
+++ python/PiFinder/pointing_model/pointing_model.py
@@ -68,3 +68,146 @@ def get_quat_angular_diff(q1, q2):
     
     return d_theta  # In radians
 
+
+class ImuDeadReckoning():
+    """
+    Use the plate-solved coordinates and IMU measurements to estimate the
+    pointing using plate solving when available or dead-reckoning using the IMU
+    when plate solving isn't available (e.g. when the scope is moving or 
+    between frames).
+
+    All angles are in radians.
+
+    EXAMPLE:
+    # Set up:
+    pointing_tracker = ImuDeadReckoning('flat')
+    pointing_tracker.set_alignment(q_scope2cam)
+    
+    # Update with plate solved and IMU data:
+    pointing_tracker.update_plate_solve_and_imu(az_solved, alt_solve, q_x2imu)
+    q_hor2scope = pointing_tracker.get_q_hor2scope()
+    
+    # Dead-reckoning using IMU
+    pointing_tracker.update_imu(q_x2imu)
+    q_hor2scope = pointing_tracker.get_q_hor2scope()
+    az, alt = get_azalt_of_q_hor2frame(q_hor2frame)
+    """
+
+    def __init__(self, screen_direction):
+        """ """
+        # IMU-to-camera orientation. Fixed by PiFinder type
+        self._set_screen_direction(screen_direction)
+
+        # Alignment:
+        self.q_scope2cam = None
+        self.q_cam2scope = None
+
+        # The poinging of the camera and scope frames wrt the horizontal frame.
+        # These get updated by plate solving and IMU dead-reckoning.
+        self.q_hor2cam = None
+        # True when q_hor2cam is estimated by IMU dead-reckoning. 
+        # False when set by plate solving
+        self.dead_reckoning = False  
+
+        # The IMU's unkonwn drifting reference frame X. This is solved for 
+        # every time we have a simultaneous plate solve and IMU measurement.
+        self.q_hor2x = None
+    
+    def set_alignment(self, 
+                      q_scope2cam: quaternion.quaternion):
+        """
+        Set the alignment between the PiFinder camera center and the scope
+        pointing.
+
+        INPUTS:
+        q_scope2cam: Quaternion that rotates the scope frame to the camera frame.
+        """
+        self.q_scope2cam = q_scope2cam.normalized()
+        self.q_cam2scope = self.q_scope2cam.conj()  
+
+    def update_plate_solve_and_imu(self, 
+                           solved_az: float, 
+                           solved_alt: float, 
+                           q_x2imu: quaternion.quaternion):
+        """ 
+        Update the state with the az/alt measurements from plate solving in the
+        camera frame. If the IMU measurement (which should be taken at the same 
+        time) is available, q_x2imu (the unknown drifting reference frame) will
+        be solved for. 
+
+        INPUTS:
+        solved_az: [rad] Azimuth of the camera pointing from plate solving.
+        solved_alt: [rad] Alt of the camera pointing from plate solving.
+        q_x2imu: [quaternion] Raw IMU measurement quaternions. This is the IMU 
+            frame orientation wrt unknown drifting reference frame X.
+        """
+        # Currently assumes that the camera is right way up on a perfect
+        # altaz mount. TODO: Generalize to rotated camera
+        self.q_hor2cam = get_q_hor2frame(solved_az, solved_alt).normalized()
+        self.dead_reckoning = False
+
+        # Calculate the IMU's unknown reference frame X using the plate solved 
+        # coordinates and IMU measurements taken from the same time. If the IMU
+        # measurement isn't provided (e.g. None or zeros), the existing q_hor2x
+        # will continue to be used.
+        if q_x2imu:
+            self.q_hor2x = self.q_hor2cam * self.q_cam2imu * q_x2imu.conj()
+            self.q_hor2x = self.q_hor2x.normalized()
+
+    def update_imu(self, 
+                   q_x2imu: quaternion.quaternion):
+        """
+        Update the state with the raw IMU measurement. Does a dead-reckoning
+        estiamte of the camera and scope pointing.
+
+        INPUTS:
+        q_x2imu: Quaternion of the IMU orientation w.r.t. an unknown and drifting
+            reference frame X used by the IMU.
+        """
+        if self.q_hor2x is not None:
+            # Dead reckoning estimate by IMU if q_hor2x has been estimated by a 
+            # previous plate solve.
+            self.q_hor2cam = self.q_hor2x * q_x2imu * self.q_imu2cam
+            self.q_hor2cam = self.q_hor2cam.normalized()
+
+            self.dead_reckoning = True
+
+    def get_q_hor2scope(self):
+        """ """
+        if self.q_hor2cam and self.q_cam2scope:
+            q_hor2scope = self.q_hor2cam * self.q_cam2scope
+            return q_hor2scope
+        else:
+            None
+
+    def get_cam_azalt(self):
+        """ """
+        az_rad, alt_rad = get_azalt_of_q_hor2frame(self.q_hor2cam)
+        return az_rad, alt_rad, self.dead_reckoning
+
+    def get_scope_azalt(self):
+        """ """
+        NotImplementedError()
+
+    def reset(self):
+        """
+        Resets the internal state.
+        """
+        self.q_hor2x = None
+
+    def _set_screen_direction(self, screen_direction: str):
+        """
+        Sets the screen direction which determines the fixed orientation between
+        the IMU and camera (q_imu2cam).
+        """
+        if screen_direction == "flat":
+            # Rotate -90° around y_imu so that z_imu' points along z_camera
+            q1 = np.quaternion(np.cos(-np.pi / 4), 0, np.sin(-np.pi / 4), 0)  
+            # Rotate -90° around z_imu' to align with the camera cooridnates
+            q2 = np.quaternion(np.cos(-np.pi / 4), 0, 0, np.sin(-np.pi / 4)) 
+            self.q_imu2cam = q1 * q2  # Intrinsic rotation: q1 followed by q2
+        else:
+            raise ValueError('Unsupported screen_direction')
+
+        self.q_imu2cam = self.q_imu2cam.normalized()
+        self.q_cam2imu = self.q_imu2cam.conj()
