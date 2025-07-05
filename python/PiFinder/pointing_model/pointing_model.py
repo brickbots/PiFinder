9a10120bc215988039b7a3c9c5aa67319a547628
TakKanekoGit <>
2025-07-05 21:01:18 +0100

    Rename func

--- python/PiFinder/pointing_model/pointing_model.py
+++ python/PiFinder/pointing_model/pointing_model.py
@@ -22,10 +22,10 @@ import numpy as np
 import quaternion
 
 
-def get_q_hor2scope(az, alt):
+def get_q_hor2frame(az, alt):
     """ 
-    Returns the quaternion to rotate from the horizontal frame to the scope frame
-    at coordinates (az, alt) for an ideal AltAz mount.
+    Returns the quaternion to rotate from the horizontal frame to the frame
+    (typically scope) at coordinates (az, alt) for an ideal AltAz mount.
 
     INPUTS:
     az: [rad] Azimuth of scope axis
@@ -35,20 +35,20 @@ def get_q_hor2scope(az, alt):
         * np.quaternion(np.cos((np.pi / 2 - alt) / 2), np.sin((np.pi / 2 - alt) / 2), 0, 0)
 
 
-def get_altaz_from_q_hor2scope(q_hor2scope):
+def get_azalt_of_q_hor2frame(q_hor2frame):
     """
-    Returns the (az, alt) pointing of the scope which is defined by the z axis
-    of the q_hor2scope quaternion.
+    Returns the (az, alt) pointing of the frame which is defined by the z axis
+    of the q_hor2frame quaternion.
 
     RETURNS:
     az: [rad]
     alt: [rad]
     """
     pz = np.quaternion(0, 0, 0, 1)  # Vector z represented as a pure quaternion
-    scope_axis = q_hor2scope * pz * q_hor2scope.conj()  # Returns a pure quaternion along scope axis
+    frame_axis = q_hor2frame * pz * q_hor2frame.conj()  # Returns a pure quaternion along scope axis
 
-    alt = np.pi / 2 - np.arccos(scope_axis.z)
-    az = np.pi - np.arctan2(scope_axis.y, scope_axis.x)
+    alt = np.pi / 2 - np.arccos(frame_axis.z)
+    az = np.pi - np.arctan2(frame_axis.y, frame_axis.x)
 
     return az, alt
 
@@ -67,3 +67,4 @@ def get_quat_angular_diff(q1, q2):
         d_theta = 2 * np.pi - d_theta
     
     return d_theta  # In radians
+
