d4d3cf47ae06c8ebb1b6b690db23850e8edf18b5
TakKanekoGit <>
2025-08-10 15:01:06 +0100

    Add ImuDeadReckoningEq and associated funcs for EQ frame

--- python/PiFinder/pointing_model/imu_dead_reckoning.py
+++ python/PiFinder/pointing_model/imu_dead_reckoning.py
@@ -179,3 +179,179 @@ class ImuDeadReckoningHoriz():
 
         self.q_imu2cam = self.q_imu2cam.normalized()
         self.q_cam2imu = self.q_imu2cam.conj()
+
+
+# ==== Equatorial frame version ====
+
+
+class ImuDeadReckoningEqFrame():
+    """
+    Use the plate-solved coordinates and IMU measurements to estimate the
+    pointing using plate solving when available or dead-reckoning using the IMU
+    when plate solving isn't available (e.g. when the scope is moving or 
+    between frames).
+    
+    This class uses the Equatorial frame as the reference and expect plate-solved
+    coordinates in (ra, dec).
+
+    All angles are in radians.
+
+    HOW IT WORKS:
+
+    EXAMPLE:
+    # Set up:
+    pointing_tracker = ImuDeadReckoningEqFrame('flat')
+    pointing_tracker.set_alignment(q_scope2cam)
+    
+    # Update with plate solved and IMU data:
+    pointing_tracker.update_plate_solve_and_imu(solved_cam_ra, solved_cam_dec, solved_cam_roll, q_x2imu)
+    
+    # Dead-reckoning using IMU
+    pointing_tracker.update_imu(q_x2imu)
+    """
+
+    def __init__(self, screen_direction):
+        """ """
+        # Alignment:
+        self.q_scope2cam = None  # ****Do we need this??
+        self.q_cam2scope = None
+        # IMU orientation:
+        self.q_imu2cam = None
+        self.q_cam2imu = None
+        # IMU-to-camera orientation. Fixed by PiFinder type
+        self._set_screen_direction(screen_direction)
+        # Putting them together:
+        self.q_imu2scope = None
+
+        # The poinging of the camera and scope frames wrt the Equatorial frame.
+        # These get updated by plate solving and IMU dead-reckoning.
+        self.q_eq2cam = None  # ***Do we need to keep q_eq2cam?
+
+        # True when q_eq2cam is estimated by IMU dead-reckoning. 
+        # False when set by plate solving
+        self.dead_reckoning = False  
+
+        # The IMU's unkonwn drifting reference frame X. This is solved for 
+        # every time we have a simultaneous plate solve and IMU measurement.
+        self.q_eq2x = None
+    
+    def set_alignment(self, 
+                      q_scope2cam: quaternion.quaternion):
+        """
+        Set the alignment between the PiFinder camera center and the scope
+        pointing.
+
+        INPUTS:
+        q_scope2cam: Quaternion that rotates the scope frame to the camera frame.
+        """
+        self.q_scope2cam = q_scope2cam.normalized()
+        self.q_cam2scope = self.q_scope2cam.conj()
+        self.q_imu2scope = self.q_imu2cam * self.q_cam2scope
+
+    def update_plate_solve_and_imu(self, 
+                           solved_cam_ra: Union[float, None], 
+                           solved_cam_dec: Union[float, None], 
+                           solved_cam_roll: Union[float, None],
+                           q_x2imu: Union[quaternion.quaternion, None]):
+        """ 
+        Update the state with the az/alt measurements from plate solving in the
+        camera frame. If the IMU measurement (which should be taken at the same 
+        time) is available, q_x2imu (the unknown drifting reference frame) will
+        be solved for. 
+
+        INPUTS:
+        solved_cam_az: [rad] Azimuth of the camera pointing from plate solving.
+        solved_cam_alt: [rad] Alt of the camera pointing from plate solving.
+        solved_cam_roll_offset: [rad] Roll offset of the camera frame +y ("up")
+            relative to the pole.
+        q_x2imu: [quaternion] Raw IMU measurement quaternions. This is the IMU 
+            frame orientation wrt unknown drifting reference frame X.
+        """
+        if (solved_cam_ra is None) or (solved_cam_dec is None):
+            return  # No update
+        else:
+            # Update plate-solved coord:
+            # Camera frame relative to the Equatorial frame where the +y camera
+            # frame (i.e. "up") points to the North Celestial Pole (NCP) -- i.e. 
+            # zero roll offset:
+            self.q_eq2cam  = qt.get_q_eq2cam(solved_cam_ra, solved_cam_dec, solved_cam_roll)
+            self.dead_reckoning = False
+
+        # Update IMU:
+        # Calculate the IMU's unknown reference frame X using the plate solved 
+        # coordinates and IMU measurements taken from the same time. If the IMU
+        # measurement isn't provided (e.g. None or zeros), the existing q_hor2x
+        # will continue to be used.
+        if q_x2imu:
+            self.q_eq2x = self.q_eq2cam * self.q_cam2imu * q_x2imu.conj()
+            self.q_eq2x = self.q_eq2x.normalized()
+
+    def update_imu(self, 
+                   q_x2imu: quaternion.quaternion):
+        """
+        Update the state with the raw IMU measurement. Does a dead-reckoning
+        estimate of the camera and scope pointing.
+
+        INPUTS:
+        q_x2imu: Quaternion of the IMU orientation w.r.t. an unknown and drifting
+            reference frame X used by the IMU.
+        """
+        if self.q_eq2x is not None:
+            # Dead reckoning estimate by IMU if q_hor2x has been estimated by a 
+            # previous plate solve.
+            self.q_eq2cam = self.q_eq2x * q_x2imu * self.q_imu2cam
+            self.q_eq2cam = self.q_eq2cam.normalized()
+
+            self.q_eq2scope = self.q_eq2cam * self.q_cam2scope
+            self.q_eq2scope = self.q_eq2scope.normalized()
+
+            self.dead_reckoning = True
+
+    def get_q_eq2scope(self):
+        """ """
+        if self.q_eq2cam and self.q_cam2scope:
+            q_eq2scope = self.q_eq2cam * self.q_cam2scope
+            return q_eq2scope
+        else:
+            None
+
+    def get_cam_radec(self):
+        """ 
+        Returns the (ra, dec, roll) of the camera and a Boolean dead_reckoning
+        to indicate if the estimate is from dead-reckoning (True) or from plate
+        solving (False).
+        """
+        ra, dec, roll = qt.get_radec_of_q_eq2cam(self.q_eq2cam)
+        return ra, dec, roll, self.dead_reckoning  # Angles are in radians
+
+    def get_scope_radec(self):
+        """ 
+        Returns the (ra, dec, roll) of the scope and a Boolean dead_reckoning
+        to indicate if the estimate is from dead-reckoning (True) or from plate
+        solving (False).
+        """
+        ra, dec, roll = qt.get_radec_of_q_eq2cam(self.q_eq2cam)
+        return ra, dec, roll, self.dead_reckoning  # Angles are in radians
+
+    def reset(self):
+        """
+        Resets the internal state.
+        """
+        self.q_eq2x = None
+
+    def _set_screen_direction(self, screen_direction: str):
+        """
+        Sets the screen direction which determines the fixed orientation between
+        the IMU and camera (q_imu2cam).
+        """
+        if screen_direction == "flat":
+            # Rotate -90° around y_imu so that z_imu' points along z_camera
+            q1 = np.quaternion(np.cos(-np.pi / 4), 0, np.sin(-np.pi / 4), 0)  
+            # Rotate -90° around z_imu' to align with the camera cooridnates
+            q2 = np.quaternion(np.cos(-np.pi / 4), 0, 0, np.sin(-np.pi / 4)) 
+            self.q_imu2cam = q1 * q2  # Intrinsic rotation: q1 followed by q2
+        else:
+            raise ValueError('Unsupported screen_direction')
+
+        self.q_imu2cam = self.q_imu2cam.normalized()
+        self.q_cam2imu = self.q_imu2cam.conj()
--- python/PiFinder/pointing_model/quaternion_transforms.py
+++ python/PiFinder/pointing_model/quaternion_transforms.py
@@ -56,6 +56,49 @@ def get_quat_angular_diff(q1, q2):
     return d_theta  # In radians
 
 
+# ========== Equatorial frame functions ============================
+
+def get_q_eq2cam(ra_rad, dec_rad, roll_rad):
+    """ 
+    Express the coordinates of a camera pointing at RA, Dec, Roll (in radians) 
+    in the relative to the Equatorial frame.
+    """
+    # Intrinsic rotation of q_ra followed by q_dec gives a quaternion rotation
+    # that points +z towards the boresight of the camera. +y to the left and
+    # +x down. 
+    q_ra = axis_angle2quat([0, 0, 1], ra_rad)  # Rotate frame around z (NCP)
+    q_dec = axis_angle2quat([0, 1, 0], np.pi / 2 - dec_rad)  # Rotate around y'
+
+    # Need to rotate this +90 degrees around z_cam so that +y_cam points up 
+    # and +x_cam points to the left of the Camera frame. In addition, need to
+    # account for the roll offset of the camera (zero if +y_cam points up along
+    # the great circle towards the NCP) 
+    q_roll = axis_angle2quat([0, 0, 1], np.pi / 2 + roll_rad)
+
+    # Intrinsic rotation:
+    q_eq2cam = (q_ra * q_dec * q_roll).normalized()
+    return q_eq2cam
+
+
+def get_radec_of_q_eq2cam(q_eq2cam):
+    """
+    """
+    # Pure quaternion along camera boresight
+    pz_cam = q_eq2cam * np.quaternion(0, 0, 0, 1)  * q_eq2cam.conj()
+    # Calculate RA, Dec from the camera boresight:
+    dec = np.arcsin(pz_cam.z)
+    ra = np.arctan2(pz_cam.y, pz_cam.x)
+
+    # Pure quaternion along y_cam which points to NCP when roll = 0
+    py_cam = q_eq2cam * np.quaternion(0, 0, 1, 0)  * q_eq2cam.conj()
+    # Local East and North vectors (roll is the angle between py_cam and the north vector)
+    vec_east = np.array([-np.sin(ra), np.cos(ra), 0])
+    vec_north = np.array([-np.sin(dec) * np.cos(ra), -np.sin(dec) * np.sin(ra), np.cos(dec)])
+    roll = -np.arctan2(np.dot(py_cam.vec, vec_east), np.dot(py_cam.vec, vec_north))
+
+    return ra, dec, roll
+
+
 # ========== Horizontal (altaz) frame functions ============================
 
 def get_q_hor2frame(az, alt):
