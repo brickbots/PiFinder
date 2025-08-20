684228ab1e94c320c91cc8a588f4790d0120c11b
TakKanekoGit <>
2025-08-20 16:18:00 +0100

    Ruff: quaternion_transforms.py

--- python/PiFinder/pointing_model/quaternion_transforms.py
+++ python/PiFinder/pointing_model/quaternion_transforms.py
@@ -9,25 +9,24 @@ from frame `a` to frame `c`:
 
 q_a2c = q_a2b * q_a2c
 
-NOTE: 
+NOTE:
 
 * All angles are in radians.
 * The quaternions use numpy quaternions and are scalar-first.
-* Some of the constant quaternion terms can be speeded up by not using 
+* Some of the constant quaternion terms can be speeded up by not using
 trigonometric functions.
-* The methods do not normalize the quaternions because this incurs a small 
-computational overhead. Normalization should be done manually as and when 
+* The methods do not normalize the quaternions because this incurs a small
+computational overhead. Normalization should be done manually as and when
 necessary.
 """
 
-from typing import Union  # When updated to Python 3.10+, remove and use new type hints
 import numpy as np
 import quaternion
 
 from PiFinder.pointing_model.astro_coords import RaDecRoll
 
 
-def axis_angle2quat(axis, theta:float) -> np.quaternion:
+def axis_angle2quat(axis, theta: float) -> np.quaternion:
     """
     Convert from axis-angle representation to a quaternion
 
@@ -35,46 +34,51 @@ def axis_angle2quat(axis, theta:float) -> np.quaternion:
     axis: (3,) Axis of rotation (doesn't need to be a unit vector)
     angle: Angle of rotation [rad]
     """
-    assert len(axis) == 3, 'axis should be a list or numpy array of length 3.'
+    assert len(axis) == 3, "axis should be a list or numpy array of length 3."
     # Define the vector part of the quaternion
     v = np.array(axis) / np.linalg.norm(axis) * np.sin(theta / 2)
 
     return np.quaternion(np.cos(theta / 2), v[0], v[1], v[2])
 
 
-def get_quat_angular_diff(q1:np.quaternion, q2:np.quaternion) -> float:
+def get_quat_angular_diff(q1: np.quaternion, q2: np.quaternion) -> float:
     """
     Calculates the relative rotation between quaternions `q1` and `q2`.
     Accounts for the double-cover property of quaternions so that if q1 and q2
     are close, you get small angle d_theta rather than something around 2 * np.pi.
     """
     dq = q1.conj() * q2
-    d_theta = 2 * np.arctan2(np.linalg.norm(dq.vec), dq.w)  # atan2 is more robust than using acos
+    d_theta = 2 * np.arctan2(
+        np.linalg.norm(dq.vec), dq.w
+    )  # atan2 is more robust than using acos
 
     # Account for double cover where q2 = -q1 gives d_theta = 2 * pi
     if d_theta > np.pi:
         d_theta = 2 * np.pi - d_theta
-    
+
     return d_theta  # In radians
 
 
 # ========== Equatorial frame functions ============================
 
-def get_q_eq2cam(ra_rad:float, dec_rad:float, roll_rad:float) -> np.quaternion:  # TODO: Rename to q_eq2frame?
-    """ 
-    Express the coordinates of a camera pointing at RA, Dec, Roll (in radians) 
+
+def get_q_eq2cam(
+    ra_rad: float, dec_rad: float, roll_rad: float
+) -> np.quaternion:  # TODO: Rename to q_eq2frame?
+    """
+    Express the coordinates of a camera pointing at RA, Dec, Roll (in radians)
     in the relative to the Equatorial frame.
     """
     # Intrinsic rotation of q_ra followed by q_dec gives a quaternion rotation
     # that points +z towards the boresight of the camera. +y to the left and
-    # +x down. 
+    # +x down.
     q_ra = axis_angle2quat([0, 0, 1], ra_rad)  # Rotate frame around z (NCP)
     q_dec = axis_angle2quat([0, 1, 0], np.pi / 2 - dec_rad)  # Rotate around y'
 
-    # Need to rotate this +90 degrees around z_cam so that +y_cam points up 
+    # Need to rotate this +90 degrees around z_cam so that +y_cam points up
     # and +x_cam points to the left of the Camera frame. In addition, need to
     # account for the roll offset of the camera (zero if +y_cam points up along
-    # the great circle towards the NCP) 
+    # the great circle towards the NCP)
     q_roll = axis_angle2quat([0, 0, 1], np.pi / 2 + roll_rad)
 
     # Intrinsic rotation:
@@ -82,22 +86,24 @@ def get_q_eq2cam(ra_rad:float, dec_rad:float, roll_rad:float) -> np.quaternion: 
     return q_eq2cam
 
 
-def get_radec_of_q_eq(q_eq2frame:np.quaternion) -> tuple[float, float, float]:
+def get_radec_of_q_eq(q_eq2frame: np.quaternion) -> tuple[float, float, float]:
     """
     Returns the (ra, dec, roll) angles of the quaterion rotation relative to
-    the equatorial frame. 
+    the equatorial frame.
     """
     # Pure quaternion along camera boresight
-    pz_frame = q_eq2frame * np.quaternion(0, 0, 0, 1)  * q_eq2frame.conj()
+    pz_frame = q_eq2frame * np.quaternion(0, 0, 0, 1) * q_eq2frame.conj()
     # Calculate RA, Dec from the camera boresight:
     dec = np.arcsin(pz_frame.z)
     ra = np.arctan2(pz_frame.y, pz_frame.x)
 
     # Pure quaternion along y_cam which points to NCP when roll = 0
-    py_cam = q_eq2frame * np.quaternion(0, 0, 1, 0)  * q_eq2frame.conj()
+    py_cam = q_eq2frame * np.quaternion(0, 0, 1, 0) * q_eq2frame.conj()
     # Local East and North vectors (roll is the angle between py_cam and the north vector)
     vec_east = np.array([-np.sin(ra), np.cos(ra), 0])
-    vec_north = np.array([-np.sin(dec) * np.cos(ra), -np.sin(dec) * np.sin(ra), np.cos(dec)])
+    vec_north = np.array(
+        [-np.sin(dec) * np.cos(ra), -np.sin(dec) * np.sin(ra), np.cos(dec)]
+    )
     roll = -np.arctan2(np.dot(py_cam.vec, vec_east), np.dot(py_cam.vec, vec_north))
 
     return ra, dec, roll  # In radians
@@ -109,7 +115,9 @@ def get_q_scope2cam(target_eq: RaDecRoll, cam_eq: RaDecRoll) -> np.quaternion:
     TODO: Update?
     """
     q_eq2cam = get_q_eq2cam(cam_eq.ra, cam_eq.dec, cam_eq.roll)
-    q_eq2scope = get_q_eq2cam(target_eq.ra, target_eq.dec, target_eq.roll)  # This assumes an EQ mount - We don't know the roll of the scope frame
+    q_eq2scope = get_q_eq2cam(
+        target_eq.ra, target_eq.dec, target_eq.roll
+    )  # This assumes an EQ mount - We don't know the roll of the scope frame
     q_scope2cam = q_eq2scope.conj() * q_eq2cam
     NotImplementedError  # Needs more workk
 
