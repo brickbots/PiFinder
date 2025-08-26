001da0580d1a6e82fe50f66c5ea37b566031a26c
Tak Kaneko <>
2025-08-26 20:36:43 +0200

    RaDecRoll: Add .initialize() method

--- python/PiFinder/pointing_model/astro_coords.py
+++ python/PiFinder/pointing_model/astro_coords.py
@@ -10,8 +10,9 @@ from typing import Union  # When updated to Python 3.10+, remove and use new typ
 @dataclass
 class RaDecRoll:
     """
-    Data class for equatorial coordinates defined by (RA, Dec, Roll).
-
+    Data class for equatorial coordinates defined by (RA, Dec, Roll). This
+    makes it easier to convert between radians and degrees.
+    
     The set methods allow values to be float or None but internally, None will
     be stored as np.nan so that the type is consistent. the get methods will
     return None if the value is np.nan.
@@ -23,6 +24,14 @@ class RaDecRoll:
     ra: float = np.nan
     dec: float = np.nan
     roll: float = np.nan
+    is_set = False
+
+    def initialize(self):
+        """ Initializes to unset state """
+        self.ra = np.nan
+        self.dec = np.nan
+        self.roll = np.nan
+        self.is_set = False       
 
     def set(
         self, 
@@ -30,10 +39,11 @@ class RaDecRoll:
         dec: Union[float, None], 
         roll: Union[float, None]
     ):
-        """ """
+        """ Set using radians """
         self.ra = ra if ra is not None else np.nan
         self.dec = dec if dec is not None else np.nan
         self.roll = roll if roll is not None else np.nan
+        self.is_set = True
 
     def set_from_deg(
         self,
@@ -41,24 +51,40 @@ class RaDecRoll:
         dec_deg: Union[float, None],
         roll_deg: Union[float, None],
     ):
-        """ """
+        """ Set using degrees """
         ra = np.deg2rad(ra_deg) if ra_deg is not None else np.nan
         dec = np.deg2rad(dec_deg) if dec_deg is not None else np.nan
         roll = np.deg2rad(roll_deg) if roll_deg is not None else np.nan
 
         self.set(ra, dec, roll)
 
-    def get(self) -> tuple[Union[float, None], Union[float, None], Union[float, None]]:
-        """Returns (ra, dec, roll) in radians"""
-        ra = self.ra if not np.isnan(self.ra) else None
-        dec = self.dec if not np.isnan(self.dec) else None
-        roll = self.roll if not np.isnan(self.roll) else None
+    def get(self, use_none=False) -> tuple[Union[float, None], Union[float, None], Union[float, None]]:
+        """ 
+        Returns (ra, dec, roll) in radians.  If use_none is True, returns None
+        for any unset (nan) values.
+        """
+        if use_none:
+            ra = self.ra if not np.isnan(self.ra) else None
+            dec = self.dec if not np.isnan(self.dec) else None
+            roll = self.roll if not np.isnan(self.roll) else None
+        else:
+            ra, dec, roll = self.ra, self.dec, self.roll
 
         return ra, dec, roll
 
-    def get_deg(self) -> tuple[float, float, float]:
-        """ """
-        return np.rad2deg(self.ra), np.rad2deg(self.dec), np.rad2deg(self.roll)
+    def get_deg(self, reurn_none=False) -> tuple[Union[float, None], Union[float, None], Union[float, None]]:
+        """ 
+        Returns (ra, dec, roll) in degrees. If use_none is True, returns None
+        for any unset (nan) values.
+        """
+        if reurn_none:
+            ra = np.rad2deg(self.ra) if not np.isnan(self.ra) else None
+            dec = np.rad2deg(self.dec) if not np.isnan(self.dec) else None
+            roll = np.rad2deg(self.roll) if not np.isnan(self.roll) else None
+        else:     
+            ra, dec, roll = np.rad2deg(self.ra), np.rad2deg(self.dec), np.rad2deg(self.roll)
+
+        return ra, dec, roll
 
 
 def get_initialized_solved_dict() -> dict:
