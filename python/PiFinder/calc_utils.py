b96626965c3fdd782c36b6cd4c2786f546dfe9c8
TakKanekoGit <>
2025-09-21 14:36:03 +0100

    Now shows RA difference to aim as -180 to +180 degrees rather than 0 to 360 degrees

--- python/PiFinder/calc_utils.py
+++ python/PiFinder/calc_utils.py
@@ -168,6 +168,8 @@ def aim_degrees(shared_state, mount_type, screen_direction, target):
         else:
             # EQ Mount type
             ra_diff = target.ra - solution["RA"]
+            ra_diff = (dec_diff + 180) % 360 - 180  # Convert to -180 to +180
+
             dec_diff = target.dec - solution["Dec"]
             dec_diff = (dec_diff + 180) % 360 - 180
             return ra_diff, dec_diff
