b25a3cf51dd5a5f697896eac995d0029bf127601
Richard <rich@brickbots.com>
2026-05-19 18:46:27 -0700

    Make sleep for FPS hit target rate

--- python/PiFinder/state_utils.py
+++ python/PiFinder/state_utils.py
@@ -1,13 +1,25 @@
+from typing import Optional
+
 from PiFinder.state import SharedStateObj
 import time
 
 
+_TARGET_PERIOD = 1.0 / 30.0
+_last_wake: Optional[float] = None
+
+
 def sleep_for_framerate(shared_state: SharedStateObj, limit_framerate=True) -> bool:
-    """ """
+    global _last_wake
+
     if shared_state.power_state() <= 0:
         time.sleep(0.5)
+        _last_wake = time.monotonic()
         return True
-    elif limit_framerate:
-        time.sleep(1 / 30)
 
+    if limit_framerate and _last_wake is not None:
+        remaining = _TARGET_PERIOD - (time.monotonic() - _last_wake)
+        if remaining > 0:
+            time.sleep(remaining)
+
+    _last_wake = time.monotonic()
     return False
